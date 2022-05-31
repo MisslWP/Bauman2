3f062de0463ac51b6d22b2ffdee1c1d7b7355c3c
UncleDrema <51056694+MisslWP@users.noreply.github.com>
2022-05-31 20:20:56 +0300

    Added lab 5.3

---
+++ C/lab_05_03/func_tests/scripts/pos_case.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+f_in=$1
+f_expect=$2
+f_expect_file=$4
+f_args=""
+converter="converter.exe"
+
+if [ $# -gt 2 ]; then
+    f_args=$(cat "$3")
+    file_arg=$(echo "$f_args" | grep -oE "[^ ]+$")
+    if [[ "$file_arg" == *.txt ]] || [[ "$file_arg" == *.txt.backup ]]; then
+        if [[ "$file_arg" == *.txt.backup ]]; then
+            cp "$file_arg" "${file_arg::-7}"
+            file_arg="${file_arg::-7}"
+        fi
+        # shellcheck disable=SC2001
+        new_file=$(echo "$file_arg" | sed 's/\(.*\)txt/\1bin/')
+        ./$converter -t -T int -o "$new_file" "$file_arg" &>/dev/null
+        f_args=$(echo "$f_args" | awk "{sub(\".{\"length(\$NF)\"}$\",\"$new_file\")}7")
+    fi
+fi
+
+if [[ -z "$USE_VALGRIND" ]]; then
+    eval ../../app.exe "$f_args" < "$f_in" > out.temp
+    res=$?
+else
+    eval valgrind --log-file="v.temp" ../../app.exe "$f_args" < "$f_in" > out.temp 2>/dev/null
+    res=$?
+fi
+
+if [ "$res" -ne 0 ]; then
+    exit 1
+fi
+
+./$converter -b -T int -o "$file_arg" "$new_file" &>/dev/null
+
+eval ./comparator.sh out.temp "$f_expect"
+res1=$?
+eval ./comparator.sh "$file_arg" "$f_expect_file" "easy"
+res2=$?
+
+if [ "$res1" -eq 0 ] && [ "$res2" -eq 0 ]; then
+    exit 0
+fi
+exit 1
